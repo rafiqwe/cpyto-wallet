7adef0729c5fa6b7830353f209b4c9eb4cb2d026
rafiqwe <rmlmanik502@gmail.com>
2025-12-18 21:41:13 +0600

    Updated: gsap animation

--- app/components/Hero/BottomHero.tsx
+++ app/components/Hero/BottomHero.tsx
@@ -9,35 +9,46 @@ import image4 from "@/public/images/Image-04.png";
 import image5 from "@/public/images/Image-05.png";
 import image6 from "@/public/images/Image-06.png";
 
-const BottomHero = () => {
+const BottomHero = ({
+  ImagesRef,
+  BottomRef,
+}: {
+  ImagesRef: React.RefObject<HTMLDivElement | null>;
+  BottomRef: React.RefObject<HTMLDivElement | null>;
+}) => {
   const Images = [image1, image2, image3, image4, image5, image6];
   return (
-    <div className="relative -top-25 ">
-      <div className="h-[110vh] relative ">
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 rounded-full overflow-hidden w-[600px] h-[600px] blur-[100px]">
-          <Image
-            src={glow}
-            alt="glow image"
-            fill
-            className="object-contain rounded-full"
-          />
+    <div ref={BottomRef} className="relative -top-32">
+      <div className="h-[110vh] relative flex justify-center items-center">
+        {/* Glow Effect */}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px]">
+          <div className="w-full h-full blur-[100px] rounded-full z-1 overflow-hidden">
+            <Image
+              src={glow}
+              alt="glow image"
+              fill
+              className="object-contain rounded-full"
+            />
+          </div>
         </div>
 
-        <div className="absolute hidden md:block inset-0 z-0 w-full h-full">
+        {/* Floating Images (separated from blur) */}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] z-10 hidden md:block">
           {Images.map((imag, index) => {
             const positions = [
-              "top-20 left-20",
-              "top-20 right-20",
-              "top-1/2 left-10 -translate-y-1/2",
+              "top-30 right-10",
               "top-1/2 right-10 -translate-y-1/2",
-              "bottom-40 left-20",
-              "bottom-40 right-20",
+              "bottom-30 right-10",
+              "top-1/2 left-10 -translate-y-1/2",
+              "top-30 left-10",
+              "bottom-30 left-10",
             ];
 
             return (
               <div
+                ref={ImagesRef}
                 key={index}
-                className={`absolute  w-24 h-24 ${positions[index] || "hidden"}`}
+                className={`absolute w-60 h-60 ${positions[index] || "hidden"}`}
               >
                 <Image
                   src={imag}
@@ -49,10 +60,12 @@ const BottomHero = () => {
             );
           })}
         </div>
-        <div className="z-5 ">
+
+        {/* Device Image */}
+        <div className="relative w-screen h-[107vh] max-w-[1200px] z-20">
           <Image
             src={device}
-            alt="glow image"
+            alt="device image"
             fill
             className="object-contain"
           />
