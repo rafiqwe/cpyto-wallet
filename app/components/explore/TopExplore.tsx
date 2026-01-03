97820b11a848c78ba9b73934a43471b18935d061
rafiqwe <rmlmanik502@gmail.com>
2026-01-03 21:36:43 +0600

    Updated: top explore component

--- app/components/explore/TopExplore.tsx
+++ app/components/explore/TopExplore.tsx
@@ -5,13 +5,13 @@ const TopExplore = () => {
     <div className="w-full h-full text-white pt-25 ">
       <div className="flex items-center flex-col gap-5 md:flex-row justify-between w-full ">
         <div className="basis-1/2">
-          <h1 className="text-5xl font-bold">
+          <h1 className="md:text-5xl text-3xl font-bold">
             Your ultimate Bitcoin wallet, packed with features to simplify your
             crypto journey
           </h1>
         </div>
         <div className="basis-1/2 flex items-center w-full justify-end gap-5 flex-col ">
-          <p className="text-lg text-gray-400 font-semibold w-100 ">
+          <p className="md:text-lg text-base text-gray-400 font-semibold w-100 px-5 ">
             From advanced tools to seamless navigation, we’ve designed
             everything to elevate your Bitcoin experience.
           </p>
